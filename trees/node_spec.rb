142ec0c5fc7f31190a0017ebafcc447b39b6cb70
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-09 10:56:34 -0500

    write tests for treenode new

--- trees/node_spec.rb
+++ trees/node_spec.rb
@@ -1,6 +1,18 @@
 require_relative 'node'
 
 describe Node do
+  let(:n1){Node.new('this')}
+  describe '#intitialize' do
+    it 'instantiates a new node containing value' do
+      expect(n1.value).to eq 'this'
+    end
 
+    it 'instantiates an empty collection of child nodes' do
+      expect(n1.child_nodes).to be_a ArrayList
+    end
+  end
+
+  describe '#add_child' do
+  end
 
 end
