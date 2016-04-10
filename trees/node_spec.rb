ee91b05e7123fb031ed11cf954c1a2cef24ab6bb
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-10 10:17:33 -0500

    change instance variable of tree node to children

--- trees/node_spec.rb
+++ trees/node_spec.rb
@@ -9,17 +9,17 @@ describe Node do
     end
 
     it 'instantiates an empty collection of child nodes' do
-      expect(n1.child_nodes).to be_a ArrayList
-      expect(n1.child_nodes.size).to eq 0
-      expect(n1.child_nodes.length).to eq 0
+      expect(n1.children).to be_a ArrayList
+      expect(n1.children.size).to eq 0
+      expect(n1.children.length).to eq 0
     end
   end
 
   describe '#add_child' do
     it 'adds a child node to the nodes child nodes' do
-      expect(n1.child_nodes.length).to eq 0
+      expect(n1.children.length).to eq 0
       n1.add_child(n2)
-      expect(n1.child_nodes.length).to eq 1
+      expect(n1.children.length).to eq 1
     end
 
   end
